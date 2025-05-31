a660cd587887dcd47489d14ee148ff50248d02de
Md Sowrov <antukhadem17@gmail.com>
2025-05-31 23:11:25 +0600

    Changes on logo

--- src/components/Common/Logo.tsx
+++ src/components/Common/Logo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center">
+    <div>
       <Link
         href="/"
         className="font-bold text-2xl text-gray-50 flex items-center"
